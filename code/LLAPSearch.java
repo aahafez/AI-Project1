835194abcc72f36174b7ee25d5e557260d63d678
Abdullah Abdelhafez <beedoz377@gmail.com>
2023-11-07 12:51:39 +0200

    Added variables

--- code/LLAPSearch.java
+++ code/LLAPSearch.java
@@ -1,8 +1,88 @@
 package code;
 public class LLAPSearch extends GenericSearch {
-   public static String solve(String initialState, String strategy, boolean visualize){
-    
-    
-    return "ok";
+    static int initialProsperity,
+    initialFood, initialMaterials, initialEnergy,
+    unitPriceFood, unitPriceMaterials, unitPriceEnergy,
+    amountRequestFood, delayRequestFood,
+    amountRequestMaterials, delayRequestMaterials,
+    amountRequestEnergy, delayRequestEnergy,
+    priceBUILD1, foodUseBUILD1, materialsUseBUILD1, energyUseBUILD1, prosperityBUILD1,
+    priceBUILD2, foodUseBUILD2, materialsUseBUILD2, energyUseBUILD2, prosperityBUILD2;
+    public static String solve(String initialState, String strategy, boolean visualize){
+        loadVariables(initialState);
+        return "Done with Solve";
    }
+   static void loadVariables(String initialState){
+        String[] values = initialState.split(";|,");
+
+        int index = 0;
+
+        initialProsperity = Integer.parseInt(values[index++]);
+
+        initialFood = Integer.parseInt(values[index++]);
+        initialMaterials = Integer.parseInt(values[index++]);
+        initialEnergy = Integer.parseInt(values[index++]);
+
+        unitPriceFood = Integer.parseInt(values[index++]);
+        unitPriceMaterials = Integer.parseInt(values[index++]);
+        unitPriceEnergy = Integer.parseInt(values[index++]);
+
+        amountRequestFood = Integer.parseInt(values[index++]);
+        delayRequestFood = Integer.parseInt(values[index++]);
+
+        amountRequestMaterials = Integer.parseInt(values[index++]);
+        delayRequestMaterials = Integer.parseInt(values[index++]);
+
+        amountRequestEnergy = Integer.parseInt(values[index++]);
+        delayRequestEnergy = Integer.parseInt(values[index++]);
+
+        priceBUILD1 = Integer.parseInt(values[index++]);
+        foodUseBUILD1 = Integer.parseInt(values[index++]);
+        materialsUseBUILD1 = Integer.parseInt(values[index++]);
+        energyUseBUILD1 = Integer.parseInt(values[index++]);
+        prosperityBUILD1 = Integer.parseInt(values[index++]);
+
+        priceBUILD2 = Integer.parseInt(values[index++]);
+        foodUseBUILD2 = Integer.parseInt(values[index++]);
+        materialsUseBUILD2 = Integer.parseInt(values[index++]);
+        energyUseBUILD2 = Integer.parseInt(values[index++]);
+        prosperityBUILD2 = Integer.parseInt(values[index++]);
+
+   }
+   static void printVariables(){
+    System.out.println("initialProsperity: " + initialProsperity);
+        System.out.println("initialFood: " + initialFood);
+        System.out.println("initialMaterials: " + initialMaterials);
+        System.out.println("initialEnergy: " + initialEnergy);
+        System.out.println("unitPriceFood: " + unitPriceFood);
+        System.out.println("unitPriceMaterials: " + unitPriceMaterials);
+        System.out.println("unitPriceEnergy: " + unitPriceEnergy);
+        System.out.println("amountRequestFood: " + amountRequestFood);
+        System.out.println("delayRequestFood: " + delayRequestFood);
+        System.out.println("amountRequestMaterials: " + amountRequestMaterials);
+        System.out.println("delayRequestMaterials: " + delayRequestMaterials);
+        System.out.println("amountRequestEnergy: " + amountRequestEnergy);
+        System.out.println("delayRequestEnergy: " + delayRequestEnergy);
+        System.out.println("priceBUILD1: " + priceBUILD1);
+        System.out.println("foodUseBUILD1: " + foodUseBUILD1);
+        System.out.println("materialsUseBUILD1: " + materialsUseBUILD1);
+        System.out.println("energyUseBUILD1: " + energyUseBUILD1);
+        System.out.println("prosperityBUILD1: " + prosperityBUILD1);
+        System.out.println("priceBUILD2: " + priceBUILD2);
+        System.out.println("foodUseBUILD2: " + foodUseBUILD2);
+        System.out.println("materialsUseBUILD2: " + materialsUseBUILD2);
+        System.out.println("energyUseBUILD2: " + energyUseBUILD2);
+        System.out.println("prosperityBUILD2: " + prosperityBUILD2);
+   }
+   public static void main(String[] args) {
+    String initialState0 = "17;" +
+                "49,30,46;" +
+                "7,57,6;" +
+                "7,1;20,2;29,2;" +
+                "350,10,9,8,28;" +
+                "408,8,12,13,34;";
+                solve(initialState0,"ok",false);
+                printVariables();
+   }
+
 }
