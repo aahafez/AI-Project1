7a7480324f95ea163778b87f849517fb009f11d8
Abdullah Abdelhafez <beedoz377@gmail.com>
2023-11-07 12:00:34 +0200

    Initial commit

--- code/LLAPSearch.java
+++ code/LLAPSearch.java
@@ -1,4 +1,8 @@
 package code;
 public class LLAPSearch extends GenericSearch {
-   
+   public static String solve(String initialState, String strategy, boolean visualize){
+    
+    
+    return "ok";
+   }
 }
